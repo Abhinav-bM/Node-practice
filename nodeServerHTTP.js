c99c3853c12784914171d157c5f15de8f5d35ff5
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-12-08 17:31:30 +0530

    update

--- nodeServerHTTP.js
+++ nodeServerHTTP.js
@@ -1,10 +1,25 @@
-const http = require("node:http")            // import http module
+// const http = require("node:http")            // import http module
 
-const server = http.createServer((req, res)=>{             // invoking createserver method it accept callback fucntion as its argument
-    res.writeHead(200, {"Content-Type": "text/plain"})                      // 200 is the http status code for successfulm response
-    res.end("Hello world")        
-})   
+// const server = http.createServer((req, res)=>{             // invoking createserver method it accept callback fucntion as its argument
+//     res.writeHead(200, {"Content-Type": "text/plain"})                      // 200 is the http status code for successfulm response
+//     res.end("Hello world")        
+// })   
 
-server.listen(3000, ()=>{
-    console.log("Server running on port 3000");
-})
+// server.listen(3000, ()=>{           // 3000 is port number
+//     console.log("Server running on port 3000");
+// })
+
+
+// let a = [1,2,3,4]
+
+// const [one, two, ...rest] = a;
+
+// console.log(rest);
+
+
+ function a(...numbers){
+   const b = numbers.reduce((num,values)=>num+values);
+   return b;
+ }
+
+ console.log(a(1,2,3,4,4))
